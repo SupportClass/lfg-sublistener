b31d4037ddf5fbc2d1f1dde3734410d6f9254a57
Alex Van Camp <email@alexvan.camp>
2015-01-18 17:48:00 -0600

    nodecg v0.4.0 parity

--- extension.js
+++ extension.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var fs = require('fs'),
-    squirrel = require('squirrel'),
+    irc = require('twitch-irc'),
     events = require('events'),
-    log = require('../../lib/logger/index'),
     History = require('./extension/history'),
     nodecg = {},
     util = require('util');
@@ -24,72 +23,69 @@ var Sublistener = function(extensionApi) {
     events.EventEmitter.call(this);
     var self = this;
 
-    // Lazy-load and lazy-install the twitch-irc npm package if necessary
-    squirrel('twitch-irc', function twitchIrcLoaded(err, irc) {
-        var client = new irc.client(nodecg.bundleConfig['twitch-irc']);
+    var client = new irc.client(nodecg.bundleConfig['twitch-irc']);
 
-        client.connect();
+    client.connect();
 
-        client.addListener('connected', function onConnected(address, port) {
-            var msg = '[lfg-sublistener] Listening for subscribers on ' + nodecg.bundleConfig['twitch-irc'].channels;
-            log.info(msg);
-        });
+    client.addListener('connected', function onConnected(address, port) {
+        var msg = 'Listening for subscribers on ' + nodecg.bundleConfig['twitch-irc'].channels;
+        nodecg.log.info(msg);
+    });
 
-        client.addListener('disconnected', function onDisconnected(reason) {
-            log.warn('[lfg-sublistener] DISCONNECTED:', reason);
-        });
+    client.addListener('disconnected', function onDisconnected(reason) {
+        nodecg.log.warn('DISCONNECTED:', reason);
+    });
 
-        client.addListener('reconnect', function onReconnect() {
-            log.info('[lfg-sublistener] Attempting to reconnect...');
-        });
+    client.addListener('reconnect', function onReconnect() {
+        nodecg.log.info('Attempting to reconnect...');
+    });
 
-        client.addListener('connectfail', function onConnectFail() {
-            log.error('[lfg-sublistener] Failed to connect, reached maximum number of retries');
-        });
+    client.addListener('connectfail', function onConnectFail() {
+        nodecg.log.error('Failed to connect, reached maximum number of retries');
+    });
 
-        client.addListener('limitation', function onLimitation(err) {
-            log.error('[lfg-sublistener]', err.message);
-        });
+    client.addListener('limitation', function onLimitation(err) {
+        nodecg.log.error(err.message);
+    });
 
-        client.addListener('subscription', function onSubscription(channel, username) {
-            if (!self.isDuplicate(username, channel)) {
-                history[channel].add(username);
-                self.acceptSubscription(username, channel)
-            }
-        });
-
-        client.addListener('crash', function onCrash(message, stack) {
-            log.error('[lfg-sublistener] CRASH:', message, '\n\n', stack);
-        });
-
-        if (nodecg.bundleConfig.chatevents) {
-            log.warn('[lfg-sublistener] Chat events are on, may cause high CPU usage');
-            client.addListener('chat', function onChat(channel, user, message) {
-                if (!self.isBroadcaster(user, channel) && !self.isModerator(user))
-                    return;
-
-                var parts = message.split(' ',2);
-                var cmd = parts[0];
-                var arg = parts.length > 1 ? parts[1] : null;
-
-                if (!arg)
-                    return;
-
-                switch (cmd) {
-                    case '!sendsub':
-                        if (self.isDuplicate(arg, channel)) {
-                            client.say(channel, 'That username, ' + arg + ', appears to be a duplicate. Use !sendsubforce to override.');
-                            break;
-                        }
-                    case '!sendsubforce':
-                        history[channel].add(arg);
-                        self.acceptSubscription(arg, channel);
-                        client.say(channel, 'Added ' + arg + ' as a subscriber');
-                        break;
-                }
-            });
+    client.addListener('subscription', function onSubscription(channel, username) {
+        if (!self.isDuplicate(username, channel)) {
+            history[channel].add(username);
+            self.acceptSubscription(username, channel)
         }
     });
+
+    client.addListener('crash', function onCrash(message, stack) {
+        nodecg.log.error('CRASH:', message, '\n\n', stack);
+    });
+
+    if (nodecg.bundleConfig.chatevents) {
+        nodecg.log.warn('Chat events are on, may cause high CPU usage');
+        client.addListener('chat', function onChat(channel, user, message) {
+            if (!self.isBroadcaster(user, channel) && !self.isModerator(user))
+                return;
+
+            var parts = message.split(' ',2);
+            var cmd = parts[0];
+            var arg = parts.length > 1 ? parts[1] : null;
+
+            if (!arg)
+                return;
+
+            switch (cmd) {
+                case '!sendsub':
+                    if (self.isDuplicate(arg, channel)) {
+                        client.say(channel, 'That username, ' + arg + ', appears to be a duplicate. Use !sendsubforce to override.');
+                        break;
+                    }
+                case '!sendsubforce':
+                    history[channel].add(arg);
+                    self.acceptSubscription(arg, channel);
+                    client.say(channel, 'Added ' + arg + ' as a subscriber');
+                    break;
+            }
+        });
+    }
 };
 
 // To let another bundle's extension take advantage of sublistener, we must export an event listener.
@@ -109,7 +105,7 @@ Sublistener.prototype.isDuplicate = function(username, channel) {
     try {
         isDupe = history[channel].find(username) >= 0;
     } catch(e) {
-        log.error("[lfg-sublistener] Dupe check failed, assuming not a dupe:", e.stack);
+        nodecg.log.error("Dupe check failed, assuming not a dupe:", e.stack);
     }
     return isDupe;
 };
