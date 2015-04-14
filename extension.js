35508402e3a7f1629919204838ed0adf61f75e1d
Alex Van Camp <email@alexvan.camp>
2015-04-14 11:20:05 -0500

    Use replicants, get ready for 0.6

--- extension.js
+++ extension.js
@@ -1,19 +1,14 @@
 'use strict';
 
-var irc = require('twitch-irc'),
-    events = require('events'),
-    util = require('util'),
-    path = require('path'),
-    locallydb = require('locallydb'),
-    nodecg = {},
-    hist = {};
+var irc = require('twitch-irc');
+var events = require('events');
+var util = require('util');
+var clone = require('clone');
+var nodecg = {};
+var hist = {};
 
 var MAX_LEN = 100;
 var POLL_INTERVAL = 60 * 1000;
-var DBPATH = path.resolve(__dirname, '../../db/lfg-sublistener');
-
-// load the database folder, the collections will be made/loaded later
-var db = new locallydb(DBPATH);
 
 var Sublistener = function(extensionApi) {
     nodecg = extensionApi;
@@ -71,19 +66,19 @@ var Sublistener = function(extensionApi) {
     }
 
     nodecg.bundleConfig['twitch-irc'].channels.forEach(function(channel) {
-        hist[channel] = db.collection(channel, db, false); // Disable autosave
+        hist[channel] = nodecg.Replicant(channel, { defaultValue: [] });
     });
 
     events.EventEmitter.call(this);
     var client = new irc.client(nodecg.bundleConfig['twitch-irc']);
 
-    nodecg.declareSyncedVar({
-        name: 'reconnecting',
-        initialVal: false
+    var reconnecting = nodecg.Replicant('reconnecting', {
+        defaultValue: false,
+        persistent: false
     });
 
     nodecg.listenFor('reconnect', function reconnect() {
-        nodecg.variables.reconnecting = true;
+        reconnecting.value = true;
         client.disconnect();
         client.connect();
     });
@@ -98,7 +93,7 @@ var Sublistener = function(extensionApi) {
     client.connect();
     client
         .addListener('connected', function onConnected() {
-            nodecg.variables.reconnecting = false;
+            reconnecting.value = false;
             self.log('info', 'Listening for subscribers on ' + nodecg.bundleConfig['twitch-irc'].channels);
         })
 
@@ -107,7 +102,7 @@ var Sublistener = function(extensionApi) {
         })
 
         .addListener('reconnect', function onReconnect() {
-            nodecg.variables.reconnecting = true;
+            reconnecting.value = true;
             self.log('info', 'Attempting to reconnect...');
         })
 
@@ -180,9 +175,13 @@ Sublistener.prototype.isDuplicate = function(username, channel, months) {
     var isDupe = false;
     try {
         if (months) {
-            isDupe = hist[channel].where({ name: username, months: months }).items.length > 0;
+            isDupe = hist[channel].value.some(function(currentValue) {
+                if (currentValue.name === username && currentValue.months === months) return true;
+            });
         } else {
-            isDupe = hist[channel].where({ name: username }).items.length > 0;
+            isDupe = hist[channel].value.some(function(currentValue) {
+                if (currentValue.name === username) return true;
+            });
         }
     } catch(e) {
         nodecg.log.error('Dupe check failed, assuming not a dupe:', e.stack);
@@ -213,23 +212,19 @@ Sublistener.prototype.acceptSubscription = function (username, channel, months) 
     };
 
     if (months) content.months = months;
-
-    // Okay, there's some real funny business going on here.
-    // `collection.insert` mutates the original object passed into it, so we have to clone it
-    hist[channel].insert(util._extend({}, content));
-    var items = hist[channel].items;
+    hist[channel].value.push(content);
+    var items = hist[channel].value;
     while (items.length > MAX_LEN) { // If we have more than MAX_LEN items, remove the oldest items
         items.shift();
     }
-    hist[channel].items = items;
-    hist[channel].save();
 
-    //... Likewise, other bundles that listen to this event emitter mutate `content`
-    // Since our collection keeps a permanent reference to these objects in `collection.items`,
-    // Any changes that other bundles make ripple back into our database. Fun!
-    // But since we passed a close to `collection.insert`, we avoid these ripple effects.
-    nodecg.sendMessage('subscription', content);
-    this.emit('subscription', content);
+    // Other bundles that listen to this event emitter mutate `content`
+    // Since our replicant keeps a permanent reference to these objects,
+    // any changes that other bundles make ripple back into our persisted replicant. Fun!
+    // For this reason, the event emits a clone.
+    var contentClone = clone(content);
+    nodecg.sendMessage('subscription', contentClone);
+    this.emit('subscription', contentClone);
 };
 
 Sublistener.prototype.log = function (level) {
