43b1e0b6b05f039ddbf42924b4d2299432841149
Alex Van Camp <email@alexvan.camp>
2016-03-12 19:13:26 -0600

    fix(twitchapi): wait for all extensions to load before attempting to integrate with lfg-twitchapi

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var server = require('../../../lib/server');
 var EventEmitter = require('events');
 var hist = require('./history.js');
 
@@ -22,25 +23,27 @@ module.exports = function (nodecg) {
 	}
 
 	// If lfg-twitchapi and an appropriate config are present, set up polling
-	if (nodecg.extensions.hasOwnProperty('lfg-twitchapi')) {
-		if (nodecg.config.login.twitch.scope.split(' ').indexOf('channel_subscriptions') < 0) {
-			nodecg.log.error('lfg-twitchapi found, but the current NodeCG config lacks the "channel_subscriptions" ' +
-				'scope. As a result, Twitch API polling for new subs will be disabled.');
-		} else {
-			var apiPoller = require('./api_poller')(nodecg);
-			apiPoller.on('gotSubscriptions', function (channel, subscriptions) {
-				// Go through subs in reverse, from oldest to newest
-				subscriptions.reverse().forEach(function (subscription) {
-					var username = subscription.user.name;
-					if (!hist.exists(username, channel)) {
-						emitter.acceptSubscription(username, channel);
-					}
+	server.on('extensionsLoaded', function () {
+		if (nodecg.extensions.hasOwnProperty('lfg-twitchapi')) {
+			if (nodecg.config.login.twitch.scope.split(' ').indexOf('channel_subscriptions') < 0) {
+				nodecg.log.error('lfg-twitchapi found, but the current NodeCG config lacks the' +
+					' "channel_subscriptions" scope. As a result, Twitch API polling for new subs will be disabled.');
+			} else {
+				var apiPoller = require('./api_poller')(nodecg);
+				apiPoller.on('gotSubscriptions', function (channel, subscriptions) {
+					// Go through subs in reverse, from oldest to newest
+					subscriptions.reverse().forEach(function (subscription) {
+						var username = subscription.user.name;
+						if (!hist.exists(username, channel)) {
+							emitter.acceptSubscription(username, channel);
+						}
+					});
 				});
-			});
+			}
+		} else {
+			nodecg.log.warn('lfg-twitchapi not present, API polling will not be available');
 		}
-	} else {
-		nodecg.log.warn('lfg-twitchapi not present, API polling will not be available');
-	}
+	});
 
 	/**
 	 * Emits a 'subscription' event and adds it to the history.
